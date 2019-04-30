26acca471dfc0d565f719dddc736200180f40e5a
ozihler <oliver.zihler@zuehlke.com>
2019-04-30 20:05:58 +0200

    add junit5

--- backend/src/test/kotlin/com/ozihler/createunitusecase/com.zihler.CreateUnitUseCase.kt
+++ backend/src/test/kotlin/com/ozihler/createunitusecase/com.zihler.CreateUnitUseCase.kt
@@ -1,8 +1,8 @@
 package com.ozihler.createunitusecase
 
 import com.ozihler.domain.WorkUnit
-import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class CreateUnitUseCaseTest {
 
